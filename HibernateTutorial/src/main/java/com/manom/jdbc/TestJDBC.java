fd307d1b24b22ab8ea12995c46381dd94bdb47d7
Manom <shailendra9920302728@gmail.com>
2022-01-05 00:33:41 +0530

    Hibernate Save Java object

---
+++ HibernateTutorial/src/main/java/com/manom/jdbc/TestJDBC.java
@@ -0,0 +1,40 @@
+package main.java.com.manom.jdbc;
+
+import main.java.com.manom.jdbc.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class TestJDBC {
+
+    public static void main(String[] args) {
+//        String jdbcURl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+//        String user="hbstudent";
+//        String pass="hbstudent";
+//        System.out.println("Connecting to databse - " + jdbcURl);
+
+        SessionFactory factory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            Student stdobj = new Student("Manom", "Singh", "shailendra@gmail.com");
+            Student stdobj2 = new Student("Shailendra", "Singh", "shailendras1612@gmail.com");
+
+
+            session.beginTransaction();
+            session.save(stdobj2);
+            session.getTransaction().commit();
+//            Connection conn = DriverManager.getConnection(jdbcURl, user, pass);
+//            System.out.println("Connected successfully with the DB!!!");
+            factory.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+}
