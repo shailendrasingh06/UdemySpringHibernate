471a01303b8aa36218227f425c9778ce00eb8b18
Manom <shailendra9920302728@gmail.com>
2022-01-24 00:39:49 +0530

    [Hibernate] @OneToOne Bi-directional and deleting Instuctor Object using InstructorDetail object

---
+++ hb-01-one-to-one-uni/src/com/manom/jdbc/DeleteInstructorDetailDemo.java
@@ -0,0 +1,44 @@
+package com.manom.jdbc;
+
+import com.manom.jdbc.entity.Instructor;
+import com.manom.jdbc.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteInstructorDetailDemo {
+
+    public static void main(String[] args) {
+//create the session factory using the provided configuration file
+        SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // get the current session from Session Factory
+        Session session = sessionFactory.getCurrentSession();
+        try{
+
+            session.beginTransaction();
+            int iDetailID = 3;
+
+            InstructorDetail instructorDetail = session.get(InstructorDetail.class, iDetailID);
+
+            if (instructorDetail !=null) {
+                System.out.println("found the instructor with ID = " + instructorDetail.getId());
+                // print the fetched instructor detail
+                System.out.println(instructorDetail.getInstructor().toString());
+                //now delete the object
+                System.out.println("Deleting the Instructor....");
+                session.delete(instructorDetail);
+                System.out.println("Deleted instructor details....");
+            }
+            session.getTransaction().commit();
+        }catch ( Exception e){
+            System.out.println("Some error occurred with in the fetching the Instructor Details...");
+            e.printStackTrace();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+}
