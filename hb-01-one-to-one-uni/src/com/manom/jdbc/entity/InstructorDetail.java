4c02b637aa5ccbb95cdd1a22a46d64fd09869016
Manom <shailendra9920302728@gmail.com>
2022-01-26 21:43:11 +0530

    [Hibernate] @OneToOne Cascade

--- hb-01-one-to-one-uni/src/com/manom/jdbc/entity/InstructorDetail.java
+++ hb-01-one-to-one-uni/src/com/manom/jdbc/entity/InstructorDetail.java
@@ -18,7 +18,7 @@ public class InstructorDetail {
     @Column(name = "hobby")
     private String hobby;
 
-    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL /*change this if you dont want to cascade Instructor object*/)
     private Instructor instructor;
 
 
